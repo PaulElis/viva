fa84f37c2e335d2e695a6128539de0ddad7c2755
zafinar <zafinar@gmail.com>
2018-03-29 16:34:24 -0400

    Merge2

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,8 @@ class List < ApplicationRecord
   belongs_to :activity
   has_many :list_comments
 
+
+
   accepts_nested_attributes_for :activity, reject_if: :all_blank
 
 end
--- db/migrate/20180326172730_create_lists.rb
+++ db/migrate/20180326172730_create_lists.rb
@@ -5,6 +5,7 @@ class CreateLists < ActiveRecord::Migration[5.1]
       t.string :location
       t.integer :user_id
       t.integer :activity_id
+      t.boolean :completed, default: false
 
       t.timestamps
     end
